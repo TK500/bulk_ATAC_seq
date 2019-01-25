2f2094b8d1fe2c74fc89443fd25c8e600aaefee3
Tanya Karagiannis <tanyatk@bu.edu>
2019-01-25 18:10:27 -0500

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -l
 
+#set batch arguments
+#$ -cwd
+#$ -j y
+#$ -P ${project}
+#$ -pe omp ${num_cores}
+#$ -m be
+#$ -M ${email}
+#$ -o trim.log
+#$ -N trim
+
+source ~/.bashrc
+
 module load trimmomatic/0.33
 
 for filename in ./*_R1_001.fastq.gz 
 
 do
 
-java -jar $SCC_TRIMMOMATIC_BIN/trimmomatic.jar PE  "$filename"  ./$(basename "$filename" R1_001.fastq.gz)R2_001.fastq.gz  ./$(basename "$filename" R1_001.fastq.gz)1P.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)1U.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)2P.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)2U.fq.gz   HEADCROP:12
+java -jar $SCC_TRIMMOMATIC_BIN/trimmomatic.jar PE  "$filename" -threads ${num_cores} ${directory}/$(basename "$filename" R1_001.fastq.gz)R2_001.fastq.gz  ${directory}/$(basename "$filename" R1_001.fastq.gz)1P.fq.gz  ${directory}/$(basename "$filename" R1_001.fastq.gz)1U.fq.gz  ${directory}/$(basename "$filename" R1_001.fastq.gz)2P.fq.gz  ${directory}/$(basename "$filename" R1_001.fastq.gz)2U.fq.gz   HEADCROP:12
 
 
 done
